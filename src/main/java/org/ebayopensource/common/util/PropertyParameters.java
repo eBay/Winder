ad96b146aebf86c35c6691a122fc4761ba0f0162
Shao <xshao@MacBook-Pro-3.local>
2016-11-07 12:10:26 -0800

    Add license

--- src/main/java/org/ebayopensource/common/util/PropertyParameters.java
+++ src/main/java/org/ebayopensource/common/util/PropertyParameters.java
@@ -1,8 +1,34 @@
+/**
+ * Copyright (c) 2016 eBay Software Foundation. All rights reserved.
+ *
+ * Licensed under the MIT license.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ *
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package org.ebayopensource.common.util;
 
 import java.util.*;
 
 /**
+ * Parameters on Properties
+ *
  * @author Sheldon Shao xshao@ebay.com on 10/16/16.
  * @version 1.0
  */
@@ -74,5 +100,17 @@ public class PropertyParameters extends AbstractParameters<Object> {
             });
         }
         return newSet;
+    }
+
+    @Override
+    public Parameters<Object> getParameters(String key) {
+        Object value = get(key);
+        if (value instanceof Parameters) {
+            return (Parameters)value;
+        }
+        else if (value instanceof Map) {
+            return new ParametersMap<>((Map)value);
+        }
+        return null;
     }
 }
